d3d49e4249212c44d76057ea71b3497cc80f97ef
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-01-04 15:53:35 +0530

    events and seperated files

---
+++ js/events.js
@@ -0,0 +1,147 @@
+var which = location.hash.slice(1);
+window.onload = function(){
+	$.ajax({
+		dataType: "json",
+		url: "https://googledrive.com/host/0B7gpUuZnCjpdWGFKOG5VbzExV00/events.json",
+		data: {},
+		success: function(data){
+			events.data=data;
+			$("#events").append('\
+				<div id="navbar" class="fixwrap">\
+					<center>\
+						<ul>\
+							<a href=\"home.html\"><li>Home</li></a>\
+							<a href="about_us.html"><li>About Us</li></a>\
+							<a href="events.html#proshows"><li data-name="proshows">Pro-Shows</li></a>\
+							<a href="events.html#tech"><li data-name="tech">Tech Theatre</li></a>\
+							<a href="events.html#cult"><li data-name="cult">Performing Arts</li></a>\
+							<a href="events.html#workshop"><li data-name="workshop">Workshops</li></a>\
+							<a href="social.html"><li data-name="social">Social Cause</li></a>\
+							<a href="sponsors.html"><li data-name="sponsors">Sponsors</li></a>\
+							<a href="home.html"><li data-name="home" class="nav-right"><img height="25" src="img/logo.png"></li></a>\
+							<a href="contact_us.html"><li data-name="team" class="nav-right">Contact Us</li></a>\
+							<a href="register2.html"><li data-name="register" class="nav-right">Register</li></a>\
+						</ul>\
+					</center>\
+				</div>\
+				<div id="content">\
+					<div class="sidebar"></div>\
+					<div class="details">\
+						<div class="imgdiv">\
+							<img class="img" src="">\
+						</div>\
+						<div class="title"></div>\
+						<div id="abst_hyper" class="abst"></div>\
+					</div>\
+				</div>');
+			openCntPg(which);
+		}
+	});
+	window.onhashchange = function(){
+		which = location.hash.slice(1);
+		openCntPg(which);
+	}
+};
+
+var animateOnce = function(a,b,c){
+	$(a).addClass('animated '+b).one('animationend webkitAnimationEnd oAnimationEnd', function(){
+		$(this).removeClass('animated '+b);
+		if(c!=undefined) c();
+	});
+};
+
+function openCntPg(a){
+	if(events.current!=a){
+		$("#events .img")[0].src=null;
+		$("#events .abst").html('');
+		$("#events .title").html('');
+		events.open(a);
+		for (var i = 0; i < $("#navbar ul a").length; i++) {
+			if($("#navbar ul a li")[i].dataset.name==which){
+				$($("#navbar ul a")[i]).addClass('active');
+			}else{
+				$($("#navbar ul a")[i]).removeClass('active');
+			}
+		};
+	}
+}
+
+function fillDetails(index){
+var w = events.currentJSON[index];
+	animateOnce("#events .details",'slideOutUp',function(){
+		$("#events .img")[0].src = w.img;
+		$("#events .title").html(w.title);
+		$("#events .abst").html(w.abstract);
+		if(w.rules)
+			$("#events .abst").append('<br><br><div style="font: 120% Carnevalee;color:rgb(236,220,136);">RULES</div>'+w.rules);
+		$("#events .abst").append(w.etc);
+		animateOnce("#events .imgdiv",'slideInLeft');
+		animateOnce("#events .abst",'slideInRight');
+	});
+}
+var events = {
+	current: '',
+	currentJSON: '',
+	open: function(a){
+		if(events.current){
+			if(events.current!=a){
+					animateOnce("#events .sidebar",'slideOutLeft',function(){
+						events.load(a);
+						$("#events .sidebar")[0].style.cssFloat='left';
+						$("#events .details")[0].style.cssFloat='right';
+						$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+						animateOnce("#events .sidebar",'slideInLeft');
+					});
+			}
+		}else{
+			events.current = -1;
+			events.load(a);
+				$("#events .sidebar")[0].style.cssFloat='left';
+				$("#events .details")[0].style.cssFloat='right';
+				$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+				animateOnce("#events .sidebar",'slideInLeft');
+		}
+		events.current=a;
+	},
+	load: function(a){
+		var jsonfile = '';
+		switch(a){
+			case "proshows":
+				$("#events")[0].style.background="rgba(5,10,0,0.85)";
+				jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdWGFKOG5VbzExV00/proshows.json';
+				break;
+			case "tech":
+				$("#events")[0].style.background="rgba(5,10,10,0.85)";
+				jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdWGFKOG5VbzExV00/tech.json';
+				break;
+			case "workshop":
+				$("#events")[0].style.background="rgba(5,0,10,0.85)";
+				jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdWGFKOG5VbzExV00/workshop.json';
+				break;
+			case "cult":
+				$("#events")[0].style.background="rgba(15,0,0,0.85)";
+				jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdWGFKOG5VbzExV00/cult.json';
+				break;
+		}
+		$("#events .sidebar").html("<ul></ul>");
+		$.ajax({
+			dataType: "json",
+			url: jsonfile,
+			data: {},
+			success: function(data){
+				events.currentJSON=data;
+				for(var i=0;i<data.length;i++){
+					$("#events .sidebar ul").append('<li data-id="'+i+'">'+data[i].title+'</li>');
+				};
+				fillDetails(0);
+				$($("#events .sidebar ul li")[0]).addClass('evt-sel');
+				$("#events .sidebar ul li").click(function(){
+					$("#events .sidebar ul li").removeClass('evt-sel');
+					var t = $(this);
+					fillDetails(t[0].dataset.id);
+					t.addClass('evt-sel');
+				});
+			}
+		});
+	}
+};
--- js/flowers.js
+++ js/flowers.js
@@ -1,78 +1,8 @@
 // (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))window.location=b})(navigator.userAgent||navigator.vendor||window.opera,'http://flux.src/mobile');
 // if(window.innerWidth<800) window.open('http://flux.src/mobile','_self');
 
-var preloadimgs = [
-	'img/gateclosed.png',
-	'img/gateopen.png',
-	'img/loadingbg.jpg',
-	'img/homemask.png',
-	'img/events/tech/AADC.jpg',
-	'img/events/tech/Quizzes.jpg',
-	'img/events/tech/AquaRobotics.jpg',
-	'img/events/tech/CIDCALCULATOR.jpg',
-	'img/events/tech/Struttura.jpg',
-	'img/events/tech/TailSpin.jpg',
-	'img/events/tech/Roboone.jpg',
-	'img/events/tech/MonsterWheels.jpg',
-	'img/events/tech/LicenseToKill.jpg',
-	'img/events/tech/EyeBotSoccer.jpg',
-	'img/events/tech/Enigma.jpg',
-	'img/events/tech/DivideByZero.jpg',
-	'img/events/cult/Dance.jpg',
-	'img/events/cult/Dramatics.jpg',
-	'img/events/cult/Painting.jpg',
-	'img/events/cult/shutter bug.jpg',
-	'img/events/cult/treasure raider.jpg',
-	'img/events/cult/Voice of fluxus.jpg',
-	'img/events/cult/movie making.jpg',
-	'img/team/vibhor.jpg',
-	'img/team/anupreet.jpg',
-	'img/team/deepakr.jpg',
-	'img/team/deepaks.jpg',
-	'img/team/dharmil.jpg',
-	'img/team/iap.jpg',
-	'img/team/kaushik.jpg',
-	'img/team/palani.jpg',
-	'img/team/vaibhav.jpg',
-	'img/team/yogesh.jpg',
-	'img/marathon.png',
-	'img/kreatrix.png',
-	'img/survey.jpg'
-]
-var imgs = [];
-for (var i = 0; i < preloadimgs.length; i++) {
-	imgs.push(new Image());
-	imgs[i].src=preloadimgs[i];
-};
-
-var gateevent = function(event) {
-	if(event.pageY>72*window.innerHeight/100){
-		$("#gate")[0].src = "img/gateopen.png";
-		animateOnce('#loading','tada');
-	}
-	else $("#gate")[0].src = "img/gateclosed.png";
-};
-$(window).on("mousemove",gateevent);
-
-$('#content > div').hide();
-$('#modals > div').hide();
-var teams = $(".teamtop .details").children();
-for(var i=0;i<teams.length;i++){
-    $(teams[i]).height(500);
-}
-//$('.teamtop .details > div').hide();
-//$('.teamtop .details div[data-id="0"]').show();
-$($(".teamtop .sidebar ul li")[0]).addClass('evt-sel');
 
 window.onload = function(){
-var url = document.referrer;
-var arr = url.split("/");
-if(arr[2]=="fluxus.in"){
-$('#confetti').prependTo('#home');
-$('#confetti').prependTo('#home2');
-$('#preloader').hide();
-$(window).off("mousemove",gateevent);
-}
 	$('#home').show();
 	$('#home2').show();
 	$('#loading').fadeOut(function(){
@@ -111,7 +41,6 @@ var topic = function(titletext,name,img,hoverimg){
 	this.y = 0;
 	this.dom = document.createElement("div");
 	this.dom.id = this.name+"topic";
-	this.dom.name = name;
 	this.dom.className = "topic";
 	this.dom.src = this.img.src;
 
@@ -143,16 +72,9 @@ var topic = function(titletext,name,img,hoverimg){
 	};
 	this.dom.onclick = function(e) {
 		animateOnce("#"+self.dom.id,'bounceOutUp',function(){
-			switch(self.dom.id){
-				case "themetopic" : window.location="theme.html"; break;
-				case "workshoptopic" : window.location="events.html#workshop";break;
-				case "techtopic" : window.location="events.html#tech";break;
-				case "proshowstopic" : window.location="events.html#proshows";break;
-				case "culttopic" : window.location="events.html#cult";break;
-				case "socialtopic" : window.location="social.html";break;
-				case "teamtopic" : window.location="contact_us.html";break;
-				default: alert(self.dom.id);
-			}
+			$(this).hide();
+			openCntPg(self.name);
+			$(this).show(100);
 		});
 	};
 	$(this.dom).appendTo("#home");
@@ -181,6 +103,112 @@ var updateTopicPos = function(){
 }
 updateTopicPos();
 
+var chnginCnt = false;
+function openCntPg(a){
+	if(!chnginCnt&&events.current!=a){
+		chnginCnt = true;
+		$("#events .img")[0].src=null;
+		$("#events .abst").html('');
+		$("#events .title").html('');
+		switch(a){
+			case "register":
+				$("#register").show();
+				animateOnce("#register",'bounceIn');
+				break;
+			case "tech":
+			case "cult":
+			case "workshop":
+			case "proshows":
+				events.open(a);
+				break;
+			default:
+				events.current = 0;
+				$("#content > div").hide();
+				setTimeout(function(){$("#"+a).fadeIn(300);},10);
+		}
+		chnginCnt = false;
+	}
+}
+var events = {
+	current: '',
+	currentJSON: '',
+	open: function (a){
+		if(events.current){
+			if(events.current!=a){
+		
+					animateOnce("#events .sidebar",'slideOutLeft',function(){
+						events.load(a);
+						$("#events .sidebar")[0].style.cssFloat='left';
+						$("#events .details")[0].style.cssFloat='right';
+						$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+						animateOnce("#events .sidebar",'slideInLeft');
+					});
+			}
+		}else{
+			events.current = -1;
+			$("#content > div").hide();
+			$("#events").show(0);
+			events.load(a);
+				$("#events .sidebar")[0].style.cssFloat='left';
+				$("#events .details")[0].style.cssFloat='right';
+				$("#events .sidebar")[0].style.backgroundImage = 'url(img/sidebarbg.png)';
+				animateOnce("#events .sidebar",'slideInLeft');
+		}
+		events.current=a;
+	},
+	load: function(a){
+		var jsonfile = '';
+		switch(a){
+			case "proshows":
+				$("#events")[0].style.background="rgba(5,10,0,0.85)";
+				//jsonfile='https://googledrive.com/host/0BzIWIU3qCj3beUxNVHNyQ1lOdWs/proshows.json'
+				jsonfile = 'data/proshows.json';
+				break;
+			case "tech":
+				$("#events")[0].style.background="rgba(5,10,10,0.85)";
+				//jsonfile='https://googledrive.com/host/0BzIWIU3qCj3beUxNVHNyQ1lOdWs/tech.json'
+				jsonfile = 'data/tech.json';
+				break;
+			case "workshop":
+				$("#events")[0].style.background="rgba(5,0,10,0.85)";
+				//jsonfile='https://googledrive.com/host/0BzIWIU3qCj3beUxNVHNyQ1lOdWs/workshop.json'
+				jsonfile = 'data/workshop.json';
+				break;
+			case "cult":
+				$("#events")[0].style.background="rgba(15,0,0,0.85)";
+				//jsonfile='https://googledrive.com/host/0BzIWIU3qCj3beUxNVHNyQ1lOdWs/cult.json'
+				// jsonfile = 'https://googledrive.com/host/0B7gpUuZnCjpdVHRwWVgzZGt2bWM/events.json';
+				jsonfile = 'data/cult.json';
+				break;
+		}
+		$("#events .sidebar").html("<ul></ul>");
+		// $("#events .sidebar ul").append('<li class=".evt-sel">asdasdasd</li>');
+		$.ajax({
+			dataType: "json",
+			url: jsonfile,
+			data: {},
+			success: function(data){
+				events.currentJSON=data;
+				for(var i=0;i<data.length;i++){
+					$("#events .sidebar ul").append('<li data-id="'+i+'">'+data[i].title+'</li>');
+				};
+				fillDetails(0);
+				$($("#events .sidebar ul li")[0]).addClass('evt-sel');
+				$("#events .sidebar ul li").click(function(){
+					$("#events .sidebar ul li").removeClass('evt-sel');
+					var t = $(this);
+					//$(this)[0].style.backgroundImage = 'url(img/linetop.png)';		
+					console.log(t);
+					fillDetails(t[0].dataset.id);
+					t.addClass('evt-sel');
+					// $(".evt-sel").animate({marginTop: t.style.top});
+				
+				});
+			}
+		});
+	}
+};
+
 $('.teamtop li[data-id]').click(function(){
 	$(".teamtop .sidebar ul li").removeClass('evt-sel');
 	var a=$(this)[0].dataset.id;
@@ -195,9 +223,11 @@ var index = Math.floor(($('.teamtop .details').scrollTop()+100)/500);
 $($(".teamtop .sidebar ul li")[index]).addClass('evt-sel');
 });
 
+/*
 $(window).resize(function(){
-	updateTopicPos();
+	if(window.innerWidth<800) updateTopicPos();
 });
+*/
 $("#regForm").submit(function(){
 	$.post('ajax/register.php', $(this).serializeArray(), function(data) {
 		alert(data);
